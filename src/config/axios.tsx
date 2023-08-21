8b498348bc88fdeb1c63e0038986f688504b9dde
Muhammed shifin <muhammedshifin424@gmail.com>
2023-08-21 17:18:34 +0530

    changed the base url

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
  //const BASE_URL = 'http://localhost:5000/api/v1/' ;
- const BASE_URL ='https://passcrafter-x.netlify.app/api/v1/'
+ const BASE_URL ='https://passcrafter-x.onrender.com/api/v1/'
+
 
 const instance = axios.create({
     baseURL: BASE_URL,
